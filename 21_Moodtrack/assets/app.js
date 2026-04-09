1d0045c74c3fe9aa9f086e496d5c695add177684
mateus trindade <mateustrindade0411@gmail.com>
2026-04-09 09:31:37 -0300

    att

--- 21_Moodtrack/assets/app.js
+++ 21_Moodtrack/assets/app.js
@@ -84,7 +84,7 @@ angular
         const yyyy = d.getFullYear();
         const mm = String(d.getMonth() + 1).padStart(2, "0");
         const dd = String(d.getDate()).padStart(2, "0");
-        return `${yyyy}-${mm}-${dd}`;
+        return `${dd}/${mm}/${yyyy}`;
       }
 
       function buildEmptyForm() {
